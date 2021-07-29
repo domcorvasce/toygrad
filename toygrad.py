737d772dec2dcdeab3f6e5522a641581097ddad8
Dom Corvasce <dcorvasce@yahoo.com>
2021-07-29 20:38:12 +0200

    chore: lint code

--- toygrad.py
+++ toygrad.py
@@ -1,10 +1,24 @@
 from typing import SupportsFloat
 
+
 class dual:
-    def __init__(self, real: SupportsFloat, grad = None, var = False):
+    """Implements a basic type for dual numbers.
+    See https://en.wikipedia.org/wiki/Dual_number.
+    """
+
+    def __init__(self, real: SupportsFloat, grad: SupportsFloat = None, var=False):
+        """Returns a new dual number.
+
+        Args:
+            real: The real part of the dual number.
+            grad: The gradient of the dual number.
+            var: Whether the dual number stands for a variable instead of a constant.
+        Returns:
+            A dual number.
+        """
         self.real = real
         self.grad = grad
-        self.var  = var
+        self.var = var
 
         if grad is None:
             self.grad = 1.0 if var else 0.0
@@ -20,17 +34,21 @@ class dual:
         return dual(self.real * other.real, derivative)
 
     def __div__(self, other):
-        derivative = (1 / (other.real ** 2)) * (other.real * self.grad - self.real * other.grad)
+        derivative = (1 / (other.real ** 2)) * (
+            other.real * self.grad - self.real * other.grad
+        )
         return dual(self.real / other, derivative)
 
     def __pow__(self, other: float):
         return dual(self.real ** other, other * self.real ** (other - 1))
 
     def __repr__(self) -> str:
-        return '{}'.format(self.real)
+        return "{}".format(self.real)
+
 
 def grad(fn, point: dual):
     return fn(point).grad
+
 
 assert grad(lambda x: x + dual(3.0), dual(3.0, var=True)) == 1
 assert grad(lambda x: x ** 2, dual(3.0, var=True)) == 6.0
